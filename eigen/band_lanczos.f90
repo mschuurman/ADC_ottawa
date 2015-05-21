d6098047a13d037f78d213592b8271268b3924bd
spneville <sneville.sneville@gmail.com>
2015-05-20 20:42:14 -0400

    Error in the previous commit fixed

--- eigen/band_lanczos.f90
+++ eigen/band_lanczos.f90
@@ -844,12 +844,8 @@
       do k=1,dim
          read(lanunit) lvec
          count=0
-
          do v=dim-n+1,dim
             count=count+1
-
-            print*,count
-            
             do m=1,matdim
                ritzvec(m,count)=ritzvec(m,count)+lvec(m)*umat(k,v)
             enddo
